ce844745046101405cd0a796f6df432c6809ad0a
Alex Scornavacca <alex.scornavacca6@gmail.com>
2024-05-24 15:25:12 -0300

    configuracion de redux

---
+++ pruebatecnicaraona/src/redux/movieSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState ={
+    title:"",
+    date:"",
+
+
+
+};
+
+export const movieSlice = createSlice({
+    name: 'movie' ,
+    initialState,
+    reducers:{
+        getMovies:( state, action) => {
+            const{ title, date} = action.payload;
+            state.title = title;
+            state.date = date;
+            
+        },
+
+    }
+});
+
+export const {getMovies} = movieSlice.actions;
+export default movieSlice.reducer;
