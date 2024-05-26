223e8d6927ce491b1262684de6549887d7a897a7
Alex Scornavacca <alex.scornavacca6@gmail.com>
2024-05-26 19:35:44 -0300

    configuracion de estilos

--- pruebatecnicaraona/src/index.jsx
+++ pruebatecnicaraona/src/index.jsx
@@ -10,10 +10,14 @@ import Main from './Main';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    
     <Provider store={store}> 
+    <div className='container'> 
       <Main/>
+      </div>
     </Provider> 
   </React.StrictMode>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
