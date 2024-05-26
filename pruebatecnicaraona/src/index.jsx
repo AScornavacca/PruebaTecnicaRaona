99efd47a51e30a68715ee3517aa3fe2bc0d3dc27
Alex Scornavacca <alex.scornavacca6@gmail.com>
2024-05-26 12:10:35 -0300

    correccion de errores

--- pruebatecnicaraona/src/index.jsx
+++ pruebatecnicaraona/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.sass';
+import './index.scss';
 import {Provider} from 'react-redux';
 import {store} from './redux/store';
 import reportWebVitals from './reportWebVitals';
